6f4fc635c1b3f354d1e62e4e57383e5b75b31255
Whiskeyi <825906196@qq.com>
2022-04-16 18:29:38 +0800

    4.16 coding

--- 柯里化.js
+++ 柯里化.js
@@ -1,4 +1,4 @@
-function multiFn(x, y, z) {
+/* function multiFn(x, y, z) {
     console.log(x * y * z);
     return x * y * z
 }
@@ -12,7 +12,7 @@ function curry(fn, args=[]) {
             return fn.apply(this, newArgs)
         }
     }
-}
+} */
 
 /* // ES6 写法
 const curry = fn =>
@@ -21,10 +21,33 @@ const curry = fn =>
             ? fn(...args)
             : (arg2) => judge(...args, arg2) */
 
-let multi = curry(multiFn)
+/* let multi = curry(multiFn)
 multi(2, 3, 4)
 multi(2)(3)(4)
 multi(2, 3)(4)
 multi(2)(3, 4)   // 以上结果都是 3，柯里化将参数拆开自由绑定，结果不变。
 let seniorMulti = multi(2) // seniorMulti 可以多次使用
-seniorMulti(3)(4) // 当我们觉得重复传递参数 2 总是冗余时，可以这样。
+seniorMulti(3)(4) // 当我们觉得重复传递参数 2 总是冗余时，可以这样。 */
+
+/* function curryIt(fn) {
+    return function fun(a) {
+        if(fn.length === 1) return fn(a)
+        fn = fn.bind(this, a)
+        return fun
+    }
+}
+
+var fn = function (a, b, c) {return a + b + c};
+console.log(curryIt(fn)(1)(2)(3)); */
+
+function sum(a) {
+    return function (b) {
+        return function(c) {
+            return a + b + c;
+        }
+    }
+}
+// 调用
+let sum1 = sum(1);
+let sum2 = sum1(2);
+console.log(sum2(3)); // 6
